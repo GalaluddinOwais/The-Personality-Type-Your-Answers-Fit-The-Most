442572ba0a049672fad5972f750920c860b7b787
Galaluddin Owais <leonilandrismessi@gmail.com>
2023-09-05 13:49:56 +0200

    first commit

---
+++ source/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+<font size=5>are you a male? هل أنت ذكر؟</font>
+<form action="q2.php" method="POST">
+<input type="radio" name="ans1" value=1> <font size=5>yes</font>
+<input type="radio" name="ans1" value=0 checked> <font size=5>no</font>
+<p>  <input type="submit" value="confirm" > </p>
+</form>
+
+
+  </body>
+</html>
